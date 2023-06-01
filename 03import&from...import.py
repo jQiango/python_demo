68f2eacffa66c01b2ad1214447ec39d3da4c97d4
wangjianqiang012 <wangjianqiang012@ke.com>
2023-06-02 01:08:57 +0800

    comimt4

---
+++ 03import&from...import.py
@@ -0,0 +1,23 @@
+# 在 python 用 import 或者 from...import 来导入相应的模块。
+
+# 将整个模块(somemodule)导入，格式为： import somemodule
+
+# 从某个模块中导入某个函数,格式为： from somemodule import somefunction
+
+# 从某个模块中导入多个函数,格式为： from somemodule import firstfunc, secondfunc, thirdfunc
+
+# 将某个模块中的全部函数导入，格式为： from somemodule import *
+# import print
+
+# from helloword import sys,str
+import sys
+print('================Python import mode==========================')
+print ('命令行参数为:')
+print ('循环打印arg参数:')
+for i in sys.argv:
+    print (i)
+    
+print ('\n python 路径为',sys.path) 
+from sys import argv,path  #  导入特定的成员
+print('================python from import===================================')
+print('path:',path) # 因为已经导入path成员，所以此处引用时不需要加sys.path
